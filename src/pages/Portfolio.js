9f366560a1df016e8ea2246d1997bf79a9e308d3
Alfonce Nzioka <alfonce.starfish@gmail.com>
2024-04-14 13:00:05 -0700

    some fixes

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,21 +1,11 @@
-import Navigation from "../components/navigation";
 import Introduction from "../components/introduction";
 import PortfolioCards from "../components/PortfolioCards";
-import FAQs from "../components/faq";
 
 export default function Home() {
-    // this is just a comment
-
-    // components to create:
-    // TopSectioComponent, About, FAQs, Investment Holdings,
-    return (
-        <>
-            <Introduction />
-            <div className="bg">
-                <PortfolioCards />
-
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Introduction />
+      <PortfolioCards />
+    </>
+  );
 }
--- src/pages/Strategy.js
+++ src/pages/Strategy.js
@@ -1,134 +1,179 @@
 export default function Strategy() {
-    return (
-        <div>
-            <div className="about">
-                <div className="col-sm-8 text-center">
-                    <h1>
-                        Wheat Penny Capital's Strategy
-                    </h1>
-                    <p>Wheat Penny Capital invests in a concentrated portfolio of exceptional companies whose long-term value is underappreciated by the market. Our portfolio is comprised of both dividend-paying bellwether businesses and overlooked contrarian companies.</p>
-                </div>
-            </div>
-            <div className="bg">
-                <div className="container">
-                    <div className="row">
-                        <div className="two">
-                            <p>Wheat Penny Capital has, as proven in the last years, a good, effective strategy that does not fail to beat the market.  First, I believe in the benefits of a concentrated portfolio, with a maximum of 10 to 12 stocks.  Rather than dabbling in 25 or 30 stocks, I aim to make knowledgeable and thoughtful investments in a small number of companies that I know well.  Secondly,  Wheat Penny Capital takes a more prudent approach and holds stocks for 5-10 years.  Unless shares deeply decline with little hope of returning, Wheat Penny Capital invests consistently in companies that are likely to do well.  Wheat Penny Capital also is not worried about one-time negative hits, as long as they happen to be good stocks that I believe they will do well.  As said before, we look towards the future, and we believe that if we choose right these businesses will bounce back before long.  Wheat Penny Capital also prefers to be familiar with the stocks we pick.  We will most likely not be investing in crypto, but expect to see WPC investing in Apple, Blackstone, Southwest, and household names.  Finally, WPC prefers to invest in stocks that pay dividends, as this shows that the companies have a steady cash flow that gives them the option to pay cash to their investors.  This is Wheat Penny Capital's strategy for stock picking, and it has proven to be efficient and effective.</p>
-
-                        </div>
-
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="row">
-                        <div className="can">
-                            <h5>Concentrated Portfolio</h5>
-                            <p>I believe in the benefits of a concentrated portfolio -- a maximum of 10 to 12 stocks. I make fewer investments in companies I know well.</p>
-                        </div>
-                        <div className="can">
-                            <h5>Long Term</h5>
-                            <p> I buy companies for the next 5 or 10 years, or longer. I do not buy for the short term.</p>
-                        </div>
-                        <div className="can">
-                            <h5>Contrarian</h5>
-                            <p> I invest opportunistically if stocks are out of favor for one-time issues. Even great companies make mistakes..</p>
-                        </div>
-                        <div className="can">
-                            <h5>Bellwethers</h5>
-                            <p> I invest in stocks I know. I prefer to own companies and brands that are household names.</p>
-                        </div>
-                        <div className="can">
-                            <h5>Dividend Payers</h5>
-                            <p> I like steady cash flow, and I like dividends. Dividends provide income that can be reinvested.
-                            </p>
-                        </div>
-                        <div className="can">
-                            <h5>Expected Returns</h5>
-                            <p> Our strategy offers a compelling middle ground in terms of returns. While it might not chase the extreme highs of aggressive growth strategies, it aims for robust, consistent returns that outpace market averages. </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="two">
-                        <h5>
-                            Here are a couple of the stocks Wheat Penny Capital has chosen to invest in
-                        </h5>
-                    </div>
-                </div>
-                <div className="container">
-                    <div className="row justify-content-center">
-                        <div className="nac">
-                            <h5>
-                                Blackstone Inc.
-                            </h5>
-                            <p>
-                                Blackstone Inc. is an asset management firm, the largest in the world. Its capital under management increased from $975 Billion at the start of 2023, to over $1 Trillion at the end of 2023. Some of its main sectors are Real Estate, Private Equity, and Credit and Insurance.
-                            </p>
-                        </div>
-                        <div className="nac">
-                            <h5>
-                                Liquidia Corp.
-                            </h5>
-                            <div className="why">
-                                Liquidia Corporation is a biotech firm that recently developed a drug that greatly improves the treatment of Pulmonary Arterial Hypertension disease. Over the past few years LQDA was in a lawsuit with United Therapeutics, who claimed that LQDA infringed on their patent. A month ago LQDA won the lawsuit and should be able to distribute their drug in the coming months.
-                            </div>
-                        </div>
-                        <div className="nac">
-                            <h5>
-                                Apollo Global Management
-                            </h5>
-                            <p>
-                                Apollo (APO), similar to Blackstone, is an asset management firm. Its assets under
-                                management grew from $548 Billion at the end of 2022 to $631 Billion, more than a 15%
-                                increase from the previous year.
-                            </p>
-                        </div>
-                        <div className="nac">
-                            <h5>
-                                Patria Investments
-                            </h5>
-                            <p>
-                                Patria (PAX), is a private markets investment firm that specializes in Latin America. It is
-                                the largest infrastructure investor in Latin America, as well as one of the largest in Real Estate and Private equity in that area.
-                            </p>
-                        </div>
-                        <div className="nac">
-                            <h5>
-                                Apple Inc.
-                            </h5>
-                            <p>
-                                Apple Inc. (AAPL), designs, manufactures and sells smartphones, computers, tablets, ect.
-                                AAPL dominates the technology sector and is currently the largest company in the world. It has a
-                                very devoted client base and has incredible pricing power.
-                            </p>
-                        </div>
-                        <div className="nac">
-                            <h5>
-                                NOW Inc.
-                            </h5>
-                            <p>
-                                NOW Inc. (DNOW),  supplies petroleum refineries, LNG
-                                terminals, and industrial manufacturing operations around the world with industrial equipment.
-                                DNOW has been in the oil and gas business for over 150 years.
-                            </p>
-                        </div>
-                        <div className="nac">
-                            <h5>
-                                Church & Dwight
-                            </h5>
-                            <p>
-                                Church & Dwight (CHD), is a homegoods and consumer staples business. CHD owns
-                                and operates many familiar brands like Arm & Hammer and Oxiclean, along with many more.
-                                CHD is constantly acquiring smaller brands and expanding their reach.
-                            </p>
-                        </div>
-
-
-                    </div>
-                </div>
- 
-            </div>
+  return (
+    <div>
+      <div className="about">
+        <div className="col-sm-8 text-center">
+          <h1>Wheat Penny Capital's Strategy</h1>
+          <p>
+            Wheat Penny Capital invests in a concentrated portfolio of
+            exceptional companies whose long-term value is underappreciated by
+            the market. Our portfolio is comprised of both dividend-paying
+            bellwether businesses and overlooked contrarian companies.
+          </p>
         </div>
-
-    )
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="two">
+            <p>
+              Wheat Penny Capital has, as proven in the last years, a good,
+              effective strategy that does not fail to beat the market. First, I
+              believe in the benefits of a concentrated portfolio, with a
+              maximum of 10 to 12 stocks. Rather than dabbling in 25 or 30
+              stocks, I aim to make knowledgeable and thoughtful investments in
+              a small number of companies that I know well. Secondly, Wheat
+              Penny Capital takes a more prudent approach and holds stocks for
+              5-10 years. Unless shares deeply decline with little hope of
+              returning, Wheat Penny Capital invests consistently in companies
+              that are likely to do well. Wheat Penny Capital also is not
+              worried about one-time negative hits, as long as they happen to be
+              good stocks that I believe they will do well. As said before, we
+              look towards the future, and we believe that if we choose right
+              these businesses will bounce back before long. Wheat Penny Capital
+              also prefers to be familiar with the stocks we pick. We will most
+              likely not be investing in crypto, but expect to see WPC investing
+              in Apple, Blackstone, Southwest, and household names. Finally, WPC
+              prefers to invest in stocks that pay dividends, as this shows that
+              the companies have a steady cash flow that gives them the option
+              to pay cash to their investors. This is Wheat Penny Capital's
+              strategy for stock picking, and it has proven to be efficient and
+              effective.
+            </p>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="can">
+              <h5>Concentrated Portfolio</h5>
+              <p>
+                I believe in the benefits of a concentrated portfolio -- a
+                maximum of 10 to 12 stocks. I make fewer investments in
+                companies I know well.
+              </p>
+            </div>
+            <div className="can">
+              <h5>Long Term</h5>
+              <p>
+                {" "}
+                I buy companies for the next 5 or 10 years, or longer. I do not
+                buy for the short term.
+              </p>
+            </div>
+            <div className="can">
+              <h5>Contrarian</h5>
+              <p>
+                {" "}
+                I invest opportunistically if stocks are out of favor for
+                one-time issues. Even great companies make mistakes..
+              </p>
+            </div>
+            <div className="can">
+              <h5>Bellwethers</h5>
+              <p>
+                {" "}
+                I invest in stocks I know. I prefer to own companies and brands
+                that are household names.
+              </p>
+            </div>
+            <div className="can">
+              <h5>Dividend Payers</h5>
+              <p>
+                {" "}
+                I like steady cash flow, and I like dividends. Dividends provide
+                income that can be reinvested.
+              </p>
+            </div>
+            <div className="can">
+              <h5>Expected Returns</h5>
+              <p>
+                {" "}
+                Our strategy offers a compelling middle ground in terms of
+                returns. While it might not chase the extreme highs of
+                aggressive growth strategies, it aims for robust, consistent
+                returns that outpace market averages.{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="two">
+            <h5>
+              Here are a couple of the stocks Wheat Penny Capital has chosen to
+              invest in
+            </h5>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="nac">
+              <h5>Blackstone Inc.</h5>
+              <p>
+                Blackstone Inc. is an asset management firm, the largest in the
+                world. Its capital under management increased from $975 Billion
+                at the start of 2023, to over $1 Trillion at the end of 2023.
+                Some of its main sectors are Real Estate, Private Equity, and
+                Credit and Insurance.
+              </p>
+            </div>
+            <div className="nac">
+              <h5>Liquidia Corp.</h5>
+              <div className="why">
+                Liquidia Corporation is a biotech firm that recently developed a
+                drug that greatly improves the treatment of Pulmonary Arterial
+                Hypertension disease. Over the past few years LQDA was in a
+                lawsuit with United Therapeutics, who claimed that LQDA
+                infringed on their patent. A month ago LQDA won the lawsuit and
+                should be able to distribute their drug in the coming months.
+              </div>
+            </div>
+            <div className="nac">
+              <h5>Apollo Global Management</h5>
+              <p>
+                Apollo (APO), similar to Blackstone, is an asset management
+                firm. Its assets under management grew from $548 Billion at the
+                end of 2022 to $631 Billion, more than a 15% increase from the
+                previous year.
+              </p>
+            </div>
+            <div className="nac">
+              <h5>Patria Investments</h5>
+              <p>
+                Patria (PAX), is a private markets investment firm that
+                specializes in Latin America. It is the largest infrastructure
+                investor in Latin America, as well as one of the largest in Real
+                Estate and Private equity in that area.
+              </p>
+            </div>
+            <div className="nac">
+              <h5>Apple Inc.</h5>
+              <p>
+                Apple Inc. (AAPL), designs, manufactures and sells smartphones,
+                computers, tablets, ect. AAPL dominates the technology sector
+                and is currently the largest company in the world. It has a very
+                devoted client base and has incredible pricing power.
+              </p>
+            </div>
+            <div className="nac">
+              <h5>NOW Inc.</h5>
+              <p>
+                NOW Inc. (DNOW), supplies petroleum refineries, LNG terminals,
+                and industrial manufacturing operations around the world with
+                industrial equipment. DNOW has been in the oil and gas business
+                for over 150 years.
+              </p>
+            </div>
+            <div className="nac">
+              <h5>Church & Dwight</h5>
+              <p>
+                Church & Dwight (CHD), is a homegoods and consumer staples
+                business. CHD owns and operates many familiar brands like Arm &
+                Hammer and Oxiclean, along with many more. CHD is constantly
+                acquiring smaller brands and expanding their reach.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
